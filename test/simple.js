8ae688e0a6c2499a2c9d3dcfa785d7d51e6db6e5
myzqy <1308250571@qq.com>
2018-06-14 17:46:25 +0800

    提交代码

---
+++ test/simple.js
@@ -0,0 +1,19 @@
+// const {should, expect, assert} = require('chai');
+// // const assert = require('assert');
+// const {add, mul} = require('../src/math');
+
+// // if (add(2, 3) === 5) {
+// //     console.log('add(2, 3) === 5 ok')
+// // } else{
+// //     console.log('add(2, 3) !== 5, error')
+// // }
+
+// // assert.equal(add(2, 3), 5);
+
+// should();
+
+// // add(2, 3).should.equal(5);
+// // expect(add(2, 3)).to.be(5);
+// // expect(add).to.be.a('string');
+
+// assert.equal(add(2, 3), 5);
